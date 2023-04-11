2b1ef4a182454b6d76118c5e77a216262c29e8bc
wangyw15 <wangyanwei2012@sina.com>
2023-04-12 00:46:18 +0800

    Steam插件初版

---
+++ steam.py
@@ -0,0 +1,86 @@
+from nonebot import get_driver, on_command, on_regex
+from nonebot.adapters import Message, Event, Bot
+from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
+from nonebot.rule import Rule
+from pydantic import BaseModel, validator
+import requests
+
+# different bots
+try:
+    import nonebot.adapters.onebot.v11 as ob11
+except:
+    ob11 = None
+try:
+    import nonebot.adapters.onebot.v12 as ob12
+except:
+    ob12 = None
+try:
+    import nonebot.adapters.kaiheila as kook
+except:
+    kook = None
+try:
+    import nonebot.adapters.mirai2 as mirai2
+except:
+    mirai2 = None
+
+# config
+class SteamConfig(BaseModel):
+    steam_enabled: bool = True
+
+    @validator('steam_enabled')
+    def steam_enabled_validator(cls, v):
+        if not isinstance(v, bool):
+            raise ValueError('steam_enabled must be a bool')
+        return v
+
+# metadata
+__plugin_meta__ = PluginMetadata(
+    name='Steam',
+    description='Steam相关功能',
+    usage='/steam',
+    config=SteamConfig,
+)
+
+config = SteamConfig.parse_obj(get_driver().config)
+
+# plugin enabled
+async def is_enabled() -> bool:
+    return config.steam_enabled
+
+# fetch app info from appid
+def fetch_app_info(appid: int) -> dict | None:
+    resp = requests.get(f'https://store.steampowered.com/api/appdetails/?appids={appid}&l=zh-cn')
+    if resp.ok and resp.status_code == 200:
+        return resp.json()
+    return None
+
+steam = on_command('steam', aliases={'蒸汽', '蒸汽平台'}, rule=is_enabled, block=True)
+
+@steam.handle()
+async def _(bot: Bot, args: Message = CommandArg()):
+    if msg := args.extract_plain_text():
+        if msg.isdigit():
+            if appinfo := fetch_app_info(msg):
+                if appinfo.get(msg, {}).get('success', False):
+                    appinfo = appinfo[msg]['data']
+                    name = appinfo['name']
+                    desc = appinfo['short_description']
+                    img = appinfo['header_image']
+                    price = appinfo['price_overview']['initial_formatted']
+                    discounted = appinfo['price_overview']['final_formatted']
+                    discount_percentage = appinfo['price_overview']['discount_percent']
+                    if ob11:
+                        resp_msg = ob11.MessageSegment.image(img) + f'\n名称: {name}\n简介: {desc}'
+                        if price != discounted:
+                            resp_msg += f'\n原价: {price}\n现价: {discounted}\n折扣: {discount_percentage}%'
+                        else:
+                            resp_msg += f'\n价格: {price}'
+                        resp_msg += '\n链接: https://store.steampowered.com/app/{msg}'
+                        await steam.finish(resp_msg)
+                else:
+                    await steam.finish('未找到该游戏')
+            else:
+                await steam.finish('请求失败')
+        else:
+            await steam.finish('请输入正确的游戏ID')
