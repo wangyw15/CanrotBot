738c951c4bd570a7d6ff7c8e3bb2f440f189fedc
wangyw15 <wangyanwei15@outlook.com>
2023-05-13 13:36:15 +0800

    命名规范

--- data.py
+++ data.py
@@ -34,12 +34,12 @@ class CanrotConfig(BaseModel):
                 raise ValueError('canrot_disable_plugins must be a list of str')
         return v
 
-__driver = get_driver()
-__global_config = __driver.config
-canrot_config = CanrotConfig.parse_obj(__global_config)
-__aio_all_data: dict[str, list[list]] = {}
-__db = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'assets.db'))
-__c = __db.cursor()
+_driver = get_driver()
+_global_config = _driver.config
+canrot_config = CanrotConfig.parse_obj(_global_config)
+_aio_all_data: dict[str, list[list]] = {}
+_db = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'assets.db'))
+_c = _db.cursor()
 loaded_plugins: list[Plugin] = []
 
 def load_plugins() -> None:
@@ -51,36 +51,36 @@ def load_plugins() -> None:
 def load_all_data():
     # get all table names
     table_names: set[str] = set()
-    temp_c = __c.execute('select distinct tbl_name from sqlite_master')
+    temp_c = _c.execute('select distinct tbl_name from sqlite_master')
     for i in temp_c.fetchall():
         table_names.add(i[0])
     logger.info(f'{len(table_names)} tables found')
 
     # get all data from tables
     for table_name in table_names:
-        temp_c = __c.execute(f'select * from {table_name}')
-        __aio_all_data[table_name] = temp_c.fetchall()
-        logger.info(f'Load {len(__aio_all_data[table_name])} items from {table_name}')
+        temp_c = _c.execute(f'select * from {table_name}')
+        _aio_all_data[table_name] = temp_c.fetchall()
+        logger.info(f'Load {len(_aio_all_data[table_name])} items from {table_name}')
 
-if not __aio_all_data:
+if not _aio_all_data:
     load_all_data()
 
 def get_data(table_name: str) -> list[list]:
-    return __aio_all_data[table_name]
+    return _aio_all_data[table_name]
 
 def get_config(name: str) -> Any:
-    return __global_config.dict()[name]
+    return _global_config.dict()[name]
 
 def execute_sql(sql: str) -> list[list]:
-    temp_c = __c.execute(sql)
+    temp_c = _c.execute(sql)
     ret: list[list] = []
     col_name: list[str] = [t[0] for t in temp_c.description]
     ret.append(col_name)
     ret += temp_c.fetchall()
     return ret
 
-@__driver.on_shutdown
+@_driver.on_shutdown
 async def _():
-    __c.close()
-    __db.close()
+    _c.close()
+    _db.close()
     logger.info('Closed database')
