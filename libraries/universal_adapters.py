db448818ce7d1771c1221193e77bf27f3cdc2158
wangyw15 <wangyanwei15@outlook.com>
2023-05-13 23:44:38 +0800

    改进用户体验

--- libraries/universal_adapters.py
+++ libraries/universal_adapters.py
@@ -185,6 +185,8 @@ async def get_image_message_from_url(bot: Bot, img_url: str) -> MessageSegment |
 
 async def send_group_forward_message(content: list[str], bot: Bot, event: Event, default_bot_name: str = 'Canrot', split: str = MESSAGE_SPLIT_LINE, header: str = '') -> None:
     if is_onebot_v11(bot) or is_onebot_v12(bot):
+        if header:
+            content.insert(0, header)
         msg_nodes = generate_onebot_group_forward_message(content, await get_bot_name(event, bot, default_bot_name), bot.self_id)
         if isinstance(event, ob11.GroupMessageEvent) or isinstance(event, ob12.GroupMessageEvent):
             await bot.send_group_forward_msg(group_id=event.group_id, messages=msg_nodes)
