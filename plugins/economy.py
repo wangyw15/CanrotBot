a546d50ad886b820111d1ae14ed1fdb91aacfc6f
wangyw15 <wangyanwei15@outlook.com>
2023-05-15 15:44:51 +0800

    修复转账问题

--- plugins/economy.py
+++ plugins/economy.py
@@ -1,7 +1,6 @@
 from nonebot import on_command
 from nonebot.adapters import Bot, Event, Message
 from nonebot.params import CommandArg
-from nonebot.typing import T_State
 from nonebot.plugin import PluginMetadata
 
 from ..libraries import user, economy, universal_adapters
@@ -13,29 +12,32 @@ __plugin_meta__ = PluginMetadata(
     config=None
 )
 
-_user = on_command('economy', aliases={'e', '钱包', '银行', '经济', 'bank'}, block=True)
-@_user.handle()
-async def _(state: T_State, bot: Bot, event: Event, args: Message = CommandArg()):
+_economy = on_command('economy', aliases={'e', '钱包', '银行', '经济', 'bank'}, block=True)
+@_economy.handle()
+async def _(bot: Bot, event: Event, args: Message = CommandArg()):
+    # check if registered
     puid = universal_adapters.get_puid(bot, event)
+    if not user.puid_user_exists(puid):
+        await _economy.finish(f'puid: {puid}\n你还没有注册')
+    
+    uid = user.get_uid(puid)
     if msg := args.extract_plain_text():
-        splitted_args = [x.strip() for x in msg.split()]
-        if msg == 'info' or msg == '信息':
-            if not user.puid_user_exists(puid):
-                await _user.finish(f'puid: {puid}\n你还没有注册')
-            else:
-                uid = user.get_uid(puid)
-                await _user.finish(f'puid: {puid}\nuid: {uid}\n当前余额: {economy.get_balance(uid)} 胡萝卜片')
+        splitted_args = [x.strip().lower() for x in msg.split()]
+        if msg == 'info' or msg == '信息' or msg == 'balance' or msg == '余额':
+            await _economy.finish(f'puid: {puid}\nuid: {uid}\n当前余额: {economy.get_balance(uid)} 胡萝卜片')
         elif splitted_args[0] == 'transfer' or splitted_args[0] == '转账':
             another = splitted_args[1]
             amount = float(splitted_args[2])
             another_uid = ''
             if '_' in another:
-                another_uid = user.get_uid(puid)
+                another_uid = user.get_uid(another)
             else:
                 another_uid = another
             if not user.uid_user_exists(another_uid):
-                await _user.finish('不存在此用户')
-            uid = user.get_uid(puid)
-            economy.transfer(uid, another_uid, amount)
+                await _economy.finish('不存在此用户')
+            if economy.transfer(uid, another_uid, amount):
+                await _economy.finish(f'向 uid {another_uid} 转账 {amount} 个胡萝卜片成功')
+            else:
+                await _economy.finish(f'向 uid {another_uid} 转账 {amount} 个胡萝卜片失败')
     else:
-        await _user.finish(__plugin_meta__.usage)
+        await _economy.finish(__plugin_meta__.usage)
