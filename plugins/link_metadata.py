bf11a1b5b9c9c157ccc629cc5739e4870bc1e61b
wangyw15 <wangyanwei15@outlook.com>
2023-05-16 22:00:22 +0800

    简介保留前50字

--- plugins/link_metadata.py
+++ plugins/link_metadata.py
@@ -21,7 +21,14 @@ async def _(state: T_State, bot: Bot, event: Event):
     elif vid.startswith('av'):
         data = await link_metadata.fetch_bilibili_data(vid, 'av')
     if data:
+        # either use the first line of description or the first 50 characters of description
+        desc = data['desc']
+        splitted_desc = '\n'.join([x.strip() for x in desc.split('\n') if x.strip() != ''])
+        if len(desc) > 50:
+            desc = desc[:50] + '...'
+        # generate message
         msg = f'标题: \n{data["title"]}\nUP主: \n{data["owner"]["name"]}\n播放: {data["stat"]["view"]}\n弹幕: {data["stat"]["danmaku"]}\n点赞: {data["stat"]["like"]}\n投币: {data["stat"]["coin"]}\n简介: \n{data["desc"]}'
+        # image message
         if universal_adapters.is_onebot_v11(bot):
             await _bilibili_video.finish(universal_adapters.ob11.Message(f'[CQ:image,file={data["pic"]}]' + msg))
         if universal_adapters.is_onebot_v12(bot):
