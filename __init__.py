2e25c5157ee4bcb8d54fc287e7949f57a734a223
wangyw15 <wangyanwei15@outlook.com>
2023-06-08 00:39:03 +0800

    小改插件初始化

--- __init__.py
+++ __init__.py
@@ -18,11 +18,14 @@ __plugin_meta__ = PluginMetadata(
     config=config.CanrotConfig
 )
 
+
 def canrot_load_plugins() -> None:
-    plugins_path = Path(__file__).parent.joinpath('plugins').resolve()
+    plugins_path = (Path(__file__).parent / 'plugins').resolve()
     load_plugins(str(plugins_path))
 
+
 canrot_load_plugins()
+
 
 plugin_help = on_command('help', aliases={'帮助'}, block=True)
 @plugin_help.handle()
@@ -33,6 +36,7 @@ async def _(bot: Bot, event: Event):
             splitted_msg.append(f'{plugin.metadata.name}\n描述：{plugin.metadata.description}\n用法：{plugin.metadata.usage}')
     await send_group_forward_message(splitted_msg, bot, event, header='机器人帮助：')
     await plugin_help.finish()
+
 
 execute_asset_sql = on_command('sql_asset', aliases={'sql-asset', 'sqla', 'asql', 'asset-sql', 'asset_sql'}, block=True)
 @execute_asset_sql.handle()
@@ -53,6 +57,7 @@ async def _(bot: Bot, event: Event, args: Message = CommandArg()):
         except TypeError as e:
             await execute_data_sql.finish('SQL执行失败或没有输出\n' + str(e))
     await execute_asset_sql.finish('执行SQL失败')
+
 
 execute_data_sql = on_command('sql_data', aliases={'sql-data', 'sqld', 'dsql', 'data-sql', 'data_sql'}, block=True)
 @execute_data_sql.handle()
