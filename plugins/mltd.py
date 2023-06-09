8c96440b225a35b12a14c3e938b27229314e954d
wangyw15 <wangyanwei15@outlook.com>
2023-06-09 22:13:26 +0800

    修复mltd无法显示活动的问题

--- plugins/mltd.py
+++ plugins/mltd.py
@@ -42,22 +42,24 @@ def iso8601_to_local(iso8601: str) -> str:
 _mltd_handler = on_shell_command('mltd', aliases={'百万现场'}, block=True)
 @_mltd_handler.handle()
 async def _(args: Annotated[list[str | MessageSegment], ShellCommandArgv()]):
-    if args[0] in ['活动', 'event']:
+    if args[0] in ['活动', 'event', 'events']:
         data = await unified.util.fetch_json_data('https://api.matsurihi.me/api/mltd/v2/events?at=now')
         if data:
-            msg = ''
+            msg = '现在正在进行的活动:\n\n'
             for event in data:
-                msg += f"活动名称: {event['name']}" \
-                       f"活动类型: {event_type[event['type'] - 1]}" \
-                       f"Appeal 类型: {appeal_type[event['appealType']]}" \
-                       f"活动开始时间: {iso8601_to_local(event['schedule']['beginAt'])}" \
-                       f"活动结束时间: {iso8601_to_local(event['schedule']['endAt'])}" \
-                       f"活动页面开放时间: {iso8601_to_local(event['schedule']['pageOpenedAt'])}" \
-                       f"活动页面关闭时间: {iso8601_to_local(event['schedule']['pageClosedAt'])}" \
-                       f"活动加速开始时间: {iso8601_to_local(event['schedule']['boostBeginAt'])}" \
-                       f"活动加速结束时间: {iso8601_to_local(event['schedule']['boostEndAt'])}" \
-                       f"活动物品: {event['item']['name']}" if event['item']['name'] else '' \
-                       '\n'
-            await _mltd_handler.finish(msg)
+                msg += f"{event['name']}\n"
+                msg += f"类型: {event_type[event['type'] - 1]}\n"
+                msg += f"Appeal 类型: {appeal_type[event['appealType']]}\n"
+                msg += f"活动开始时间: {iso8601_to_local(event['schedule']['beginAt'])}\n"
+                msg += f"活动结束时间: {iso8601_to_local(event['schedule']['endAt'])}\n"
+                msg += f"页面开放时间: {iso8601_to_local(event['schedule']['pageOpenedAt'])}\n"
+                msg += f"页面关闭时间: {iso8601_to_local(event['schedule']['pageClosedAt'])}\n"
+                msg += f"加速开始时间: {iso8601_to_local(event['schedule']['boostBeginAt'])}\n" \
+                       if event['schedule']['boostBeginAt'] else ''
+                msg += f"加速结束时间: {iso8601_to_local(event['schedule']['boostEndAt'])}\n" \
+                       if event['schedule']['boostEndAt'] else ''
+                msg += f"活动物品: {event['item']['name']}" if event['item']['name'] else ''
+                msg += '\n\n'
+            await _mltd_handler.finish(msg.strip())
         else:
             await _mltd_handler.finish('现在还没有活动喵~')
