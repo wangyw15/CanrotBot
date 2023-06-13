837438953e1eaf3988dee547250c8cf56877d1f0
wangyw15 <wangyanwei15@outlook.com>
2023-06-14 02:22:02 +0800

    抽卡接近游戏内概率

--- libraries/mltd.py
+++ libraries/mltd.py
@@ -12,10 +12,11 @@ _mltd_assets_path = Path(__file__).parent.parent / 'assets' / 'mltd'
 _cards: list[dict] = []
 _cards_zh: list[dict] = []
 _cards_last_fetch: datetime | None = None
+_cards_for_gasha: dict[int, list[dict]] = {}
 
 
 async def load_cards(force_reload: bool = False) -> None:
-    global _cards, _cards_zh, _cards_last_fetch
+    global _cards, _cards_zh, _cards_last_fetch, _cards_for_gasha
     if force_reload or not _cards or not _cards_zh or not _cards_last_fetch \
             or (datetime.now() - _cards_last_fetch).total_seconds() > 3600*24:
         _cards = await unified.util.fetch_json_data(
@@ -23,6 +24,14 @@ async def load_cards(force_reload: bool = False) -> None:
         _cards_zh = await unified.util.fetch_json_data(
             'https://api.matsurihi.me/api/mltd/v2/zh/cards?includeCostumes=true&includeParameters=true&includeLines=true&includeSkills=true&includeEvents=true')
         _cards_last_fetch = datetime.now()
+        # 生成卡池
+        _cards_for_gasha = {}
+        for card in _cards:
+            if card['rarity'] == 1 or card['exType'] != 0:
+                continue
+            if card['rarity'] not in _cards_for_gasha:
+                _cards_for_gasha[card['rarity']] = []
+            _cards_for_gasha[card['rarity']].append(card)
 
 
 async def search_card(keyword: str, force_jp: bool = False) -> dict:
@@ -64,18 +73,21 @@ async def get_events(time: Literal['now'] | datetime = 'now') -> list[dict] | No
 
 async def gasha() -> Tuple[bytes, list[dict]]:
     await load_cards()
-    idols: list[dict] = []
-    while len(idols) != 10:
-        card = random.choice(_cards)
-        # 不出N卡且只出常驻卡
-        if card['rarity'] == 1 or card['exType'] != 0:
-            continue
-        idols.append(card)
+    cards: list[dict] = []
+    # 抽卡
+    while len(cards) != 10:
+        magic_number = random.random()
+        if magic_number < 0.03:
+            cards.append(random.choice(_cards_for_gasha[4]))
+        elif magic_number < 0.15:
+            cards.append(random.choice(_cards_for_gasha[3]))
+        else:
+            cards.append(random.choice(_cards_for_gasha[2]))
     with (_mltd_assets_path / 'gasha.html').open('r', encoding='utf-8') as f:
         html = f.read()
-    html = html.replace("'{DATA_HERE}'", json.dumps(idols))
+    html = html.replace("'{DATA_HERE}'", json.dumps(cards))
     img = await render_by_browser.render_html(html, _mltd_assets_path, viewport={'width': 1920, 'height': 1080})
-    return img, idols
+    return img, cards
 
 
 async def main():
