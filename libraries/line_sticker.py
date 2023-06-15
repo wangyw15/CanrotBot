116d9637959706e8753e34d545e975e553a9e0df
wangyw15 <wangyanwei15@outlook.com>
2023-06-15 22:09:32 +0800

    修复line贴纸无法获取名称的问题

--- libraries/line_sticker.py
+++ libraries/line_sticker.py
@@ -9,7 +9,7 @@ async def get_line_sticker(sticker_id: str) -> Tuple[str, bytes]:
     info_resp = await unified.util.fetch_bytes_data(f'https://store.line.me/stickershop/product/{sticker_id}/en')
     info_resp = info_resp.decode('utf-8')
     soup = BeautifulSoup(info_resp, 'html.parser')
-    sticker_name = soup.select_one('.mdCMN38Item01Ttl').text
+    sticker_name = soup.select_one('h2:nth-child(2)').text.strip()
     # 文件
     file_data = await unified.util.fetch_bytes_data(
         f'https://stickershop.line-scdn.net/stickershop/v1/product/{sticker_id}/iphone/stickerpack@2x.zip')
