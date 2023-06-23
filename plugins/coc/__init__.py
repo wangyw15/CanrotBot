fb6bf3368498088eaa81dbe78c4ae7b42f43f8eb
wangyw15 <wangyanwei15@outlook.com>
2023-06-23 17:46:38 +0800

    优化跑团骰子结果

--- plugins/coc/__init__.py
+++ plugins/coc/__init__.py
@@ -17,5 +17,8 @@ _dice_handler = on_command('dice', aliases={'骰子', 'd'}, block=True)
 @_dice_handler.handle()
 async def _(args: Message = CommandArg()):
     if expr := args.extract_plain_text():
-        result = dice.dice_expression(expr)
-        await _dice_handler.finish(expr + ' = ' + result[1] + ' = ' + str(result[0]))
+        result, calculated_expr = dice.dice_expression(expr)
+        if str(result) == calculated_expr:
+            await _dice_handler.finish(expr + ' = ' + str(result))
+        else:
+            await _dice_handler.finish(expr + ' = ' + calculated_expr + ' = ' + str(result))
