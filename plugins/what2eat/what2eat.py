adc8c1fb1c58e6295872081c271689fdea617c8d
wangyw15 <wangyanwei15@outlook.com>
2023-06-24 17:25:04 +0800

    吃什么加载数据提示

--- plugins/what2eat/what2eat.py
+++ plugins/what2eat/what2eat.py
@@ -2,9 +2,13 @@ import json
 import random
 from pathlib import Path
 
+from nonebot import logger
+
 # 加载数据
 with (Path(__file__).parent.parent.parent / "assets" / "what2eat.json").open('r', encoding='utf-8') as f:
     _what2eat_data: dict[str, list[dict]] = json.load(f)
+logger.info(f'What2eat drink: {len(_what2eat_data["drink"])}')
+logger.info(f'What2eat food: {len(_what2eat_data["food"])}')
 
 
 def get_drink_by_brand(brand: str) -> str:
