55bde0c0881af844203da43ae7b3b62c0b225a2a
wangyw15 <wangyanwei15@outlook.com>
2023-07-11 21:01:57 +0800

    更新注释

--- adapters/unified/detector.py
+++ adapters/unified/detector.py
@@ -8,6 +8,13 @@ class Detector:
     """
     @staticmethod
     def is_onebot_v11(context: Bot | Event) -> bool:
+        """
+        检测是否为 OneBot v11 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 OneBot v11 协议
+        """
         if adapters.onebot_v11:
             return isinstance(context, adapters.onebot_v11.Bot) \
                 or isinstance(context, adapters.onebot_v11.Event)
@@ -15,6 +22,13 @@ class Detector:
 
     @staticmethod
     def is_onebot_v12(context: Bot | Event) -> bool:
+        """
+        检测是否为 OneBot v12 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 OneBot v12 协议
+        """
         if adapters.onebot_v12:
             return isinstance(context, adapters.onebot_v12.Bot) \
                 or isinstance(context, adapters.onebot_v12.Event)
@@ -22,6 +36,13 @@ class Detector:
 
     @staticmethod
     def is_mirai2(context: Bot | Event) -> bool:
+        """
+        检测是否为 Mirai2 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 Mirai2 协议
+        """
         if adapters.mirai2:
             return isinstance(context, adapters.mirai2.Bot) \
                 or isinstance(context, adapters.mirai2.Event)
@@ -29,6 +50,13 @@ class Detector:
 
     @staticmethod
     def is_qqguild(context: Bot | Event) -> bool:
+        """
+        检测是否为 QQ 频道协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 QQGuild 协议
+        """
         if adapters.qqguild:
             return isinstance(context, adapters.qqguild.Bot) \
                 or isinstance(context, adapters.qqguild.Event)
@@ -36,6 +64,13 @@ class Detector:
 
     @staticmethod
     def is_kook(context: Bot | Event) -> bool:
+        """
+        检测是否为 Kook 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 Kook 协议
+        """
         if adapters.kook:
             return isinstance(context, adapters.kook.Bot) \
                 or isinstance(context, adapters.kook.Event)
@@ -43,6 +78,13 @@ class Detector:
 
     @staticmethod
     def is_console(context: Bot | Event) -> bool:
+        """
+        检测是否为 Console 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 Console 协议
+        """
         if adapters.console:
             return isinstance(context, adapters.console.Bot) \
                 or isinstance(context, adapters.console.Event)
@@ -50,15 +92,35 @@ class Detector:
 
     @staticmethod
     def is_onebot(context: Bot | Event) -> bool:
+        """
+        检测是否为 OneBot 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 OneBot 协议
+        """
         return Detector.is_onebot_v11(context) or Detector.is_onebot_v12(context)
 
     @staticmethod
     def is_qq(context: Bot | Event) -> bool:
+        """
+        检测是否为 QQ 协议
+
+        :param context: Bot | Event
+
+        :return: 是否为 QQ 协议
+        """
         return Detector.is_onebot(context) or Detector.is_mirai2(context)
 
     @staticmethod
     def can_send_image(context: Bot | Event) -> bool:
-        """检测是否可以发送图片"""
+        """
+        检测是否可以发送图片
+
+        :param context: Bot | Event
+
+        :return: 是否可以发送图片
+        """
         if Detector.is_qq(context) or Detector.is_kook(context) or Detector.is_qqguild(context):
             return True
         return False
