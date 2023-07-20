eeda217cbae1ee5fda24d4130f39a76bca56deb4
wangyw15 <wangyanwei15@outlook.com>
2023-07-20 22:43:19 +0800

    修复无法注册适配器的问题

--- adapters/unified/__init__.py
+++ adapters/unified/__init__.py
@@ -13,9 +13,9 @@ for name, key in adapters.SupportedAdapters.items():
     if not name.startswith('__') \
             and name not in config.canrot_config.canrot_disabled_adapters \
             and key not in config.canrot_config.canrot_disabled_adapters \
-            and hasattr(getattr(adapters.SupportedAdapters, name), 'Adapter'):
+            and hasattr(getattr(adapters, key + '_module'), 'Adapter'):
         try:
-            driver.register_adapter(getattr(adapters.SupportedAdapters, name).Adapter)
+            driver.register_adapter(getattr(adapters, key + '_module').Adapter)
             nonebot.logger.info('加载适配器: ' + name)
         except Exception as e:
             nonebot.logger.info(f'加载 {name} 失败: {e}')
