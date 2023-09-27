802cebbf17cd87d6a64ffed8ab5023a058b1e6df
wangyw15 <wangyanwei15@outlook.com>
2023-09-27 13:48:27 +0800

    添加定位搜索充电桩功能

--- plugins/charging_pile/__init__.py
+++ plugins/charging_pile/__init__.py
@@ -4,10 +4,11 @@ from nonebot.params import CommandArg
 from nonebot.plugin import PluginMetadata
 
 from . import charging_pile
+from adapters import unified
 
 __plugin_meta__ = PluginMetadata(
     name='找充电桩',
-    description='电瓶车没电了！我应该去哪充电？（仅限上大）',
+    description='电瓶车没电了！我应该去哪充电？',
     usage='/charge',
     config=None
 )
@@ -18,5 +19,15 @@ _charging_pile_handler = on_command('charge', aliases={'充电', '充电桩'}, b
 
 @_charging_pile_handler.handle()
 async def _():
-    await _charging_pile_handler.send('正在查询喵~')
-    await _charging_pile_handler.finish(await charging_pile.generate_message('上海大学'))
+    if not unified.Detector.is_onebot():
+        await _charging_pile_handler.finish('抱歉，目前只支持 OneBot 协议')
+    # await _charging_pile_handler.send('正在查询喵~')
+    # await _charging_pile_handler.finish(await charging_pile.generate_message('上海大学'))
+
+
+@_charging_pile_handler.got('location', prompt='请发送定位')
+async def _(loc_msg: unified.adapters.onebot_v11_module.Message):
+    if loc_msg[0].type == 'location':
+        await _charging_pile_handler.send('正在查询喵~')
+        stations = await charging_pile.get_station_list(loc_msg[0].data['lat'], loc_msg[0].data['lon'])
+        await _charging_pile_handler.finish(await charging_pile.generate_message(stations))
--- plugins/charging_pile/charging_pile.py
+++ plugins/charging_pile/charging_pile.py
@@ -4,8 +4,9 @@ import typing
 _client = AsyncClient()
 
 
-async def get_station_list() -> list[dict[str, typing.Any]]:
-    resp = await _client.get('https://api.issks.com/issksapi/V2/ec/stationList/json.shtml?mapX=121.394003&mapY=31.315989')
+async def get_station_list(longitude: float | str, latitude: float | str) -> list[dict[str, typing.Any]]:
+    resp = await _client.get(
+        f'https://api.issks.com/issksapi/V2/ec/stationList/json.shtml?mapX={longitude}&mapY={latitude}')
     if resp.is_success and resp.status_code == 200:
         return resp.json()['list']
     return []
@@ -18,9 +19,8 @@ async def get_station_plugs(station_id: int) -> list[dict[str, typing.Any]]:
     return []
 
 
-async def generate_message(keyword: str = '') -> str:
+async def generate_message(stations: list[dict[str, typing.Any]], keyword: str = '') -> str:
     ret = ''
-    stations = await get_station_list()
     for station in stations:
         if station['iHardWareState'] != 1 or keyword and keyword not in station['vStationName']:
             continue
