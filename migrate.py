bb32ad450cd1be6b86b8f3a5c9eeb97dd0199f9d
wangyw15 <wangyanwei15@outlook.com>
2023-10-07 00:19:37 +0800

    修复迁移工具无法使用的问题

--- migrate.py
+++ migrate.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 import json
-from sqlalchemy import insert, select
+from sqlalchemy import insert
 from datetime import datetime
 
 import nonebot
 nonebot.init()
+
+# 前置插件
+nonebot.load_plugin('nonebot_plugin_apscheduler')
+
+# 基础插件
+essentials_plugins_path = (Path(__file__).parent / 'essentials' / 'plugins').resolve()
+nonebot.load_plugins(str(essentials_plugins_path))
+
+# 普通插件
+plugins_path = (Path(__file__).parent / 'plugins').resolve()
+nonebot.load_plugins(str(plugins_path))
+
 from storage import config, database
 
 
