76b5061ac2a0c141f368ef24035babb1bda38164
wangyw15 <wangyanwei15@outlook.com>
2023-11-19 16:11:40 +0800

    更改邦邦目录结构

--- plugins/bangdream/__init__.py
+++ plugins/bangdream/__init__.py
@@ -1,5 +1,5 @@
-import random
 import difflib
+import random
 from typing import Annotated
 
 from nonebot import on_shell_command
@@ -7,8 +7,8 @@ from nonebot.adapters import MessageSegment
 from nonebot.params import ShellCommandArgv
 from nonebot.plugin import PluginMetadata
 
-from . import bangdream
 from adapters import unified
+from . import bestdori
 
 # TODO 记得改
 __plugin_meta__ = PluginMetadata(
@@ -27,7 +27,7 @@ async def _(args: Annotated[list[str | MessageSegment], ShellCommandArgv()]):
     if len(args) == 0:
         await _bangdream_handler.finish('想要什么喵？')
     if args[0].lower() in ['comic', 'comics', '漫画', '小漫画']:
-        comics = await bangdream.get_comic_list()
+        comics = await bestdori.comic.get_comic_list()
         if len(args) == 1:  # 随机所有小漫画
             comic_id = random.choice(list(comics.keys()))
         elif len(args) == 2 and args[1] in ['四格']:  # 随机四格
@@ -50,11 +50,10 @@ async def _(args: Annotated[list[str | MessageSegment], ShellCommandArgv()]):
             if comic_id not in comics:
                 await _bangdream_handler.finish('没有找到这个漫画喵')
         # 发送漫画
-        title, _ = bangdream.get_content_by_language(comics[comic_id]['title'])
+        title, _ = bestdori.comic.get_content_by_language(comics[comic_id]['title'])
         if unified.Detector.can_send_image():
-            content, _ = await bangdream.get_comic(comic_id)
+            content, _ = await bestdori.comic.get_comic(comic_id)
             await _bangdream_handler.finish(unified.MessageSegment.text(title) +
                                             unified.MessageSegment.image(content))
         else:
             await _bangdream_handler.finish(f'{title}\nhttps://bestdori.com/info/comics/{comic_id}')
-
