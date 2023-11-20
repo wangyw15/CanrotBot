cb48f9c7b06787e6185989ccf71505423a0d86dc
wangyw15 <wangyanwei15@outlook.com>
2023-11-20 22:34:06 +0800

    番剧使用新适配器

--- plugins/anime.py
+++ plugins/anime.py
@@ -1,19 +1,14 @@
-import re
-from typing import Annotated
-
-from nonebot import on_shell_command
-from nonebot.adapters import MessageSegment, Bot, Event
-from nonebot.params import ShellCommandArgv
+from arclet.alconna import Option, Args
 from nonebot.plugin import PluginMetadata
+from nonebot_plugin_alconna import on_alconna, Alconna, AlconnaQuery, Query, UniMsg, Text, Image
 
-from adapters import unified
 from essentials.libraries import util
 from libraries import anime
 
 __plugin_meta__ = PluginMetadata(
     name='番剧工具',
     description='是个提供番剧相关的插件，但是现在只提供番剧搜索功能',
-    usage='/<anime|动漫|番剧> <关键词>',
+    usage='/<番剧> <关键词>',
     config=None
 )
 
@@ -48,63 +43,27 @@ def generate_message_from_anime_data(name: str, data: dict, possibility: float) 
     return msg
 
 
-async def _search_anime_by_image(img_url: str, bot: Bot, event: Event) -> None:
-    if img_url:
-        search_resp = await anime.search_anime_by_image(img_url)
-        if search_resp and not search_resp.get('error'):
-            tracemoe_data = search_resp['result'][0]
-            data = anime.search_anime_by_anilist_id(tracemoe_data['anilist'])
-            final_msg = unified.Message()
-            # 视觉图
-            final_msg.append(unified.MessageSegment.image(data['picture'], '封面图'))
-            # 生成消息
-            if data:
-                final_msg.append(generate_message_from_anime_data('', data, tracemoe_data["similarity"]))
-            # tracemoe 消息
-            # 视频截图
-            final_msg.append(unified.MessageSegment.image(tracemoe_data['image'], '封面图'))
-            # 剩下的 tracemoe 消息
-            final_msg.append(f'trace.moe 信息:\n'
-                             f'番剧文件名: {tracemoe_data["filename"]}\n'
-                             f'第 {tracemoe_data["episode"]} 集\n'
-                             f'时间: {util.seconds_to_time(tracemoe_data["from"])}~'
-                             f'{util.seconds_to_time(tracemoe_data["to"])}\n'
-                             f'AniList 链接: https://anilist.co/anime/{tracemoe_data["anilist"]}\n')
-            await final_msg.send()
-            return
-    await bot.send(event, '搜索失败')
+_command = on_alconna(Alconna(
+    '番剧',
+    Option(
+        'search',
+        Args['anime_query', str],
+        alias=['搜索', '查番', '番剧', '动漫'],
+    ),
+), block=True)
 
 
-_anime_handler = on_shell_command('anime', aliases={'动漫', '番剧'}, block=True)
+@_command.assign('search')
+async def _(anime_query: Query[str] = AlconnaQuery('anime_query')):
+    name, data, possibility = anime.search_anime_by_name(anime_query.result.strip())
+    text_msg = generate_message_from_anime_data(name, data, possibility)
+    msg = UniMsg()
+    if await util.can_send_segment(Image):
+        msg.append(Image(url=data['picture']))
+    msg.append(Text(text_msg))
+    await _command.finish(msg)
 
 
-@_anime_handler.handle()
-async def _(bot: Bot, event: Event, args: Annotated[list[str | MessageSegment], ShellCommandArgv()]):
-    if len(args) == 2:
-        if args[0].lower() == 'search' or args[0] == '搜索':
-            # 从不同的地方获取信息
-            search_keyword = await unified.adapters.get_adapter().parse_message(
-                unified.adapters.message.Message(args[1]))
-            img_url = ''
-            if search_keyword[0].type == unified.MessageSegmentTypes.IMAGE:
-                img_url = search_keyword[0].data['file']
-            elif isinstance(args[1], unified.adapters.kook_module.MessageSegment):
-                text = ''
-                if args[1].type == 'kmarkdown':
-                    text = args[1].data['raw_content']
-                elif args[1].type == 'text':
-                    text = args[1].data['content']
-                if text:
-                    img_url = re.search(r'\[.*]\((\S+)\)', text).groups()[0]
-            elif util.is_url(args[1]):
-                img_url = str(args[1])
-            if img_url:
-                await _search_anime_by_image(img_url, bot, event)
-                await _anime_handler.finish()
-            name, data, possibility = anime.search_anime_by_name(' '.join(args[1:]))
-            text_msg = generate_message_from_anime_data(name, data, possibility)
-            msg = unified.Message()
-            msg.append(unified.MessageSegment.image(data['picture'], f'{name} 封面图'))
-            msg.append(text_msg)
-            await msg.send()
-            await _anime_handler.finish()
+@_command.handle()
+async def _():
+    await _command.finish(__plugin_meta__.usage)
