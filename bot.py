136c9fd5c2783a3f21e812e06c6487ee1e94e0b8
wangyw15 <wangyanwei15@outlook.com>
2023-11-21 00:34:09 +0800

    增加日志记录

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 
 import nonebot
@@ -7,23 +8,33 @@ import nonebot.adapters.mirai2 as mirai2
 import nonebot.adapters.onebot.v11 as onebot_v11
 import nonebot.adapters.onebot.v12 as onebot_v12
 import nonebot.adapters.qqguild as qqguild
+from nonebot import logger
+from nonebot.log import default_format
 
 # 初始化
 nonebot.init(alconna_use_command_start=True)
 driver = nonebot.get_driver()
 
+# 保存日志
+from storage import config
+logger_path = Path(config.canrot_config.canrot_data_path) / 'log'
+if not logger_path.exists():
+    logger_path.mkdir(parents=True)
+logger.add(logger_path / (datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".log"),
+           level="WARNING", format=default_format, rotation="1 week")
+
 # 注册适配器
-if 'console' not in driver.config.canrot_disabled_adapters:
+if 'console' not in config.canrot_config.canrot_disabled_adapters:
     driver.register_adapter(console.Adapter)
-if 'kook' not in driver.config.canrot_disabled_adapters:
+if 'kook' not in config.canrot_config.canrot_disabled_adapters:
     driver.register_adapter(kook.Adapter)
-if 'mirai2' not in driver.config.canrot_disabled_adapters:
+if 'mirai2' not in config.canrot_config.canrot_disabled_adapters:
     driver.register_adapter(mirai2.Adapter)
-if 'onebot_v11' not in driver.config.canrot_disabled_adapters:
+if 'onebot_v11' not in config.canrot_config.canrot_disabled_adapters:
     driver.register_adapter(onebot_v11.Adapter)
-if 'onebot_v12' not in driver.config.canrot_disabled_adapters:
+if 'onebot_v12' not in config.canrot_config.canrot_disabled_adapters:
     driver.register_adapter(onebot_v12.Adapter)
-if 'qqguild' not in driver.config.canrot_disabled_adapters:
+if 'qqguild' not in config.canrot_config.canrot_disabled_adapters:
     driver.register_adapter(qqguild.Adapter)
 
 # 内置插件
