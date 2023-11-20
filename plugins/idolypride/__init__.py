d08b83b7b3292a96bd36e218cb364d49bba2c7f5
wangyw15 <wangyanwei15@outlook.com>
2023-11-20 12:55:24 +0800

    偶像荣耀使用新适配器

--- plugins/idolypride/__init__.py
+++ plugins/idolypride/__init__.py
@@ -1,10 +1,8 @@
-from typing import Annotated
 from urllib import parse
 
-from nonebot import on_shell_command
-from nonebot.adapters import MessageSegment
-from nonebot.params import ShellCommandArgv
+from arclet.alconna import Alconna
 from nonebot.plugin import PluginMetadata
+from nonebot_plugin_alconna import on_alconna, Option
 
 from . import idolypride
 
@@ -16,24 +14,32 @@ __plugin_meta__ = PluginMetadata(
 )
 
 
-_ip_handler = on_shell_command('idolypride', aliases={'ip', '偶像荣耀'}, block=True)
+_command = on_alconna(Alconna(
+    '偶像荣耀',
+    Option(
+        'events',
+        alias=['calendar', '活动', '日历'],
+    )
+), block=True)
 
 
-@_ip_handler.handle()
-async def _(args: Annotated[list[str | MessageSegment], ShellCommandArgv()]):
-    if args:
-        if args[0] in ['events', 'calendar', '活动', '日历']:
-            data = await idolypride.get_today_events()
-            if data:
-                msg = '偶像荣耀现在的活动: \n\n'
-                for i in data:
-                    msg += f'{i["name"]}\n' \
-                           f'开始时间: {i["start"].strftime("%Y-%m-%d")}\n' \
-                           f'结束时间: {i["end"].strftime("%Y-%m-%d")}\n' \
-                           f'类型: {i["type"]}\n'
-                    msg += f'链接: https://wiki.biligame.com/idolypride/{parse.quote(i["page"])}\n' if i["page"] else ''
-                    msg += '\n'
-                await _ip_handler.finish(msg.strip())
-            else:
-                await _ip_handler.finish('今天没有活动喵~')
-    await _ip_handler.finish(__plugin_meta__.usage)
+@_command.assign('events')
+async def _():
+    data = await idolypride.get_today_events()
+    if data:
+        msg = '偶像荣耀现在的活动: \n\n'
+        for i in data:
+            msg += f'{i["name"]}\n' \
+                   f'开始时间: {i["start"].strftime("%Y-%m-%d")}\n' \
+                   f'结束时间: {i["end"].strftime("%Y-%m-%d")}\n' \
+                   f'类型: {i["type"]}\n'
+            msg += f'链接: https://wiki.biligame.com/idolypride/{parse.quote(i["page"])}\n' if i["page"] else ''
+            msg += '\n'
+        await _command.finish(msg.strip())
+    else:
+        await _command.finish('今天没有活动喵~')
+
+
+@_command.handle()
+async def _():
+    await _command.finish(__plugin_meta__.usage)
