f0712b0f1f017f97d06eff7faf44273867758b5a
wangyw15 <wangyanwei15@outlook.com>
2023-11-27 14:16:38 +0800

    删除已实现的TODO

--- storage/database.py
+++ storage/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session, sessionmaker
 
 from . import config
 
-# TODO: 可配置的数据库
 _engine = create_engine(config.canrot_config.canrot_database)
 _session = sessionmaker(bind=_engine)
 
