dfc3535fe870f855dfa3f352ccddd7deea3f3cd3
wangyw15 <wangyanwei15@outlook.com>
2024-01-29 19:07:05 +0800

    删除方舟抽卡时调试用的输出文件

--- plugins/arknights/__init__.py
+++ plugins/arknights/__init__.py
@@ -42,8 +42,6 @@ async def _():
         await _command.finish("你的余额不足喵~")
     # 抽卡
     img, operators = await arknights.generate_gacha(uid)
-    with open("a.png", "wb") as f:
-        f.write(img)
     # 付款提示
     await _command.send("你的二十五个胡萝卜片我就收下了喵~")
     # 生成消息
