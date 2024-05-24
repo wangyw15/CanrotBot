caef33cb83a59c91be89d022dfa4b7935f2a7edf
wangyw15 <wangyanwei15@outlook.com>
2024-05-24 21:02:11 +0800

    更新疯狂星期四

--- plugins/crazy_thursday.py
+++ plugins/crazy_thursday.py
@@ -1,6 +1,6 @@
 import random
 
-from nonebot import on_regex
+from nonebot import on_keyword
 from nonebot.plugin import PluginMetadata
 
 from storage import asset
@@ -8,15 +8,13 @@ from storage import asset
 __plugin_meta__ = PluginMetadata(
     name="疯狂星期四",
     description="随机发送疯狂星期四文案",
-    usage="疯狂星期[一|二|三|四|五|六|日|天]，也支持日文触发：狂乱[月|火|水|木|金|土|日]曜日",
+    usage="消息包含疯狂星期四就会触发",
     config=None,
 )
 
 
-crazy_thursday_posts: list[str] = asset.LocalAsset("crazy_thursday.json")["post"]
-crazy_thursday_matcher = on_regex(
-    r"疯狂星期[一二三四五六日天]|狂乱[月火水木金土日]曜日", block=True
-)
+crazy_thursday_posts: list[str] = asset.LocalAsset("crazy_thursday.json").json()
+crazy_thursday_matcher = on_keyword({"疯狂星期四"}, block=True)
 
 
 @crazy_thursday_matcher.handle()
