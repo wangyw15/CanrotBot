3b1f2de6240a8258352c210942c2b0209ccb0715
wangyw15 <wangyanwei15@outlook.com>
2024-08-03 23:02:19 +0800

    steam使用工具函数

--- plugins/steam/steam.py
+++ plugins/steam/steam.py
@@ -1,21 +1,10 @@
-from httpx import AsyncClient
-
-from storage import config
-
-
-if proxy := config.get_config("canrot_proxy"):
-    _client = AsyncClient(proxy=proxy)
-else:
-    _client = AsyncClient()
+from essentials.libraries.network import fetch_json_data
 
 
 async def fetch_app_info(
     appid: str, steam_lang: str = "zh-cn", steam_region="cn"
 ) -> dict | None:
-    resp = await _client.get(
+    return await fetch_json_data(
         f"https://store.steampowered.com/api/appdetails/?appids={appid}&l={steam_lang}&cc={steam_region}",
         headers={"Accept-Language": steam_lang},
     )
-    if resp.is_success and resp.status_code == 200:
-        return resp.json()
-    return None
