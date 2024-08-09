87e18cd88c7f688ddbbdb145903eb3f26f153685
wangyw15 <wangyanwei15@outlook.com>
2024-08-09 10:14:24 +0800

    LLM Tool 可追加自定义消息

--- llm/llm_backend/ollama_backend/__init__.py
+++ llm/llm_backend/ollama_backend/__init__.py
@@ -1,3 +1,6 @@
+import inspect
+from typing import overload
+
 import ollama
 from nonebot import get_plugin_config, logger
 from nonebot_plugin_alconna import UniMessage
@@ -10,14 +13,18 @@ ollama_config = get_plugin_config(OllamaConfig)
 ollama_client = ollama.AsyncClient(ollama_config.ollama_host)
 
 
-async def chat(text: str) -> str | UniMessage:
-    messages: list[Message] = [{"role": "user", "content": text}]
+async def chat(message: list[Message] | str) -> str | UniMessage:
+    extra_message: UniMessage = UniMessage()
+    if isinstance(message, str):
+        messages: list[Message] = [{"role": "user", "content": message}]
+    else:
+        messages = [i.copy() for i in message]
 
     response = await ollama_client.chat(
         model=ollama_config.ollama_model,
         messages=messages,
         stream=False,
-        tools=tool.tools_description + tool.custom_tools_description,
+        tools=tool.tools_description,
     )
 
     if "tool_calls" in response["message"]:
@@ -29,17 +36,23 @@ async def chat(text: str) -> str | UniMessage:
             if tool_name in tool.tools_callable:
                 logger.info(f'Called tool "{tool_name}"')
                 logger.debug(f'Tool "{tool_name}" called with arguments {tool_args}')
+
                 tool_ret = tool.tools_callable[tool_name](**tool_args)
                 logger.debug(f'Tool "{tool_name}" returned with {tool_ret}')
+
+                return_type = inspect.signature(
+                    tool.tools_callable[tool_name]
+                ).return_annotation
+                if return_type == UniMessage:
+                    return tool_ret
+                elif return_type == tuple[str, UniMessage]:
+                    extra_message.append(tool_ret[1])
+                    tool_ret = tool_ret[0]
+                elif return_type == tuple[UniMessage | str]:
+                    extra_message.append(tool_ret[0])
+                    tool_ret = tool_ret[1]
+
                 messages.append({"role": "tool", "content": str(tool_ret)})
-            elif tool_name in tool.custom_tools_callable:
-                logger.info(f'Called custom tool "{tool_name}"')
-                logger.debug(
-                    f'Custom tool "{tool_name}" called with arguments {tool_args}'
-                )
-                tool_ret = tool.custom_tools_callable[tool_name](**tool_args)
-                logger.debug(f'Custom tool "{tool_name}" returned with {tool_ret}')
-                return tool_ret
 
         response = await ollama_client.chat(
             model=ollama_config.ollama_model,
@@ -47,4 +60,6 @@ async def chat(text: str) -> str | UniMessage:
             stream=False,
         )
 
+    if len(extra_message) > 0:
+        return UniMessage.text(response["message"]["content"]) + extra_message
     return response["message"]["content"]
--- llm/tool.py
+++ llm/tool.py
@@ -61,18 +61,14 @@ def resolve_method(func: Callable) -> Tool:
 tools_description: list[Tool] = []
 tools_callable: dict[str, Callable] = {}
 
-custom_tools_description: list[Tool] = []
-custom_tools_callable: dict[str, Callable] = {}
 
-
-def check_annotation(func: Callable, return_type: type) -> bool:
+def check_annotation(func: Callable) -> type:
     """
     检查方法参数是否全部标记为 str，是否有返回值标注
 
     :param func: 方法
-    :param return_type: 返回值类型
 
-    :return: 是否通过检查
+    :return: 返回值类型
     """
     spec = inspect.getfullargspec(func)
     for arg in spec.args:
@@ -82,22 +78,29 @@ def check_annotation(func: Callable, return_type: type) -> bool:
             raise ValueError(f"Argument {arg} must be annotated as str")
     if "return" not in spec.annotations:
         raise ValueError("No type annotation found for return")
-    if spec.annotations["return"] is not return_type:
-        raise ValueError(f"Return value must be annotated as {return_type.__name__}")
-    return True
+    return spec.annotations["return"]
 
 
 def register_tool(func: Callable) -> Callable:
     """
-    装饰器，将方法注册为 Tool，需要参数和返回值都标记为 str
+    装饰器，将方法注册为 Tool，需要参数和返回值都标记为 str, UniMessage或tuple[str, UniMessage]
+
+    str 返回作为 LLM Toolcall 的返回值，UniMessage 作为追加消息发送
+
+    返回没有 str 则不会继续使用 LLM，直接使用 UniMessage 作为返回
 
     :param func: 方法
 
     :return: 方法
     """
     # 检查类型标注
-    if not check_annotation(func, str):
-        raise ValueError("Annotation check failed")
+    if check_annotation(func) not in (
+        str,
+        UniMessage,
+        tuple[str, UniMessage],
+        tuple[UniMessage | str],
+    ):
+        raise ValueError("Return type must be str or UniMessage")
 
     # 注册 Tool
     resolved = resolve_method(func)
@@ -107,27 +110,6 @@ def register_tool(func: Callable) -> Callable:
     return func
 
 
-def register_custom_tool(func: Callable) -> Callable:
-    """
-    装饰器，将方法注册为 Tool，且返回消息由 Tool 自定义，需要参数标记为 str，返回标记为 UniMessage
-
-    :param func: 方法
-
-    :return: 方法
-    """
-    # 检查类型标注
-    if not check_annotation(func, UniMessage):
-        raise ValueError("Annotation check failed")
-
-    # 注册 Custom tool
-    resolved = resolve_method(func)
-    custom_tools_description.append(resolved)
-    custom_tools_callable[resolved["function"]["name"]] = func
-    logger.info(f"Registered custom tool \"{resolved['function']['name']}\"")
-    return func
-
-
 @get_driver().on_startup
 async def log_tool_count():
     logger.info(f"Registered {len(tools_description)} tools")
-    logger.info(f"Registered {len(custom_tools_description)} custom tools")
