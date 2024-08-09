ffd3d82482ec6d2e1159b78afe32ebe8b32110ba
wangyw15 <wangyanwei15@outlook.com>
2024-08-10 00:28:53 +0800

    暂时禁用reply

--- plugins/reply/data.py
+++ plugins/reply/data.py
@@ -11,5 +11,4 @@ class ReplyConfig(database.Base):
         Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
     )
     group_id: Mapped[str] = mapped_column(Text, nullable=False)
-    enable: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     rate: Mapped[float] = mapped_column(Float, nullable=False, default=0)
--- plugins/reply/reply.py
+++ plugins/reply/reply.py
@@ -2,7 +2,6 @@ import logging
 import random
 import re
 
-import jieba
 from nonebot.adapters import Event
 from sqlalchemy import select, update, insert
 
@@ -15,34 +14,9 @@ BOT_NAME: str = "我"
 SENDER_NAME: str = "主人"
 
 # 加载数据
-_reply_data: list[dict[str, str | None]] = file.read_json(
-    path.get_asset_path() / "reply.json"
-)
-
-# 关闭jieba日志
-jieba.setLogLevel(logging.WARNING)
-
-
-def is_negative(msg: str) -> bool:
-    """
-    判断是否为否定句
-
-    :param msg: 要判断的内容
-
-    :return: 是否为否定句
-    """
-    return "不" in msg
-
-
-def is_single_word(msg: str) -> bool:
-    """
-    判断是否为单个词
-
-    :param msg: 要判断的内容
-
-    :return: 是否为单个词
-    """
-    return len(jieba.lcut(msg)) == 1
+# _reply_data: list[dict[str, str | None]] = file.read_json(
+#     path.get_asset_path() / "reply.json"
+# )
 
 
 def is_regex_pattern(content: str) -> bool:
@@ -65,41 +39,7 @@ def generate_response(msg: str, fallback_keyword: bool = True) -> str:
 
     :return: 回复内容
     """
-    responses = []
-    cut_msg = jieba.lcut(msg)
-
-    for reply_item in _reply_data:
-        pattern = reply_item["pattern"]
-        response = reply_item["response"]
-        # character = reply_item['character']
-        if is_regex_pattern(pattern):
-            pattern = pattern[1:-1]
-            if re.search(pattern, msg):
-                responses.append(response)
-        elif pattern == msg:
-            responses.append(response)
-        elif is_negative(pattern) == is_negative(msg) and pattern in cut_msg:
-            responses.append(response)
-        elif not is_single_word(pattern) and len(cut_msg) != 0 and pattern in msg:
-            responses.append(response)
-    if responses:
-        return random.choice(responses)
-
-    if not fallback_keyword:
-        return ""
-
-    # 关键词回复
-    for reply_item in _reply_data:
-        pattern = reply_item["pattern"]
-        response = reply_item["response"]
-        # character = reply_item['character']
-        if (not is_regex_pattern(pattern)) and pattern in msg:
-            responses.append(response)
-    if responses:
-        return random.choice(responses)
-
-    # 默认回复
-    return UNKNOWN_RESPONSE
+    raise NotImplementedError("未实现")
 
 
 def get_reply_rate(group_id: str) -> float:
@@ -155,34 +95,6 @@ def check_reply(event: Event) -> bool:
     if group_id := util.get_group_id(event):  # 确保是群消息
         query = select(data.ReplyConfig).where(data.ReplyConfig.group_id.is_(group_id))
         with database.get_session().begin() as session:
-            enable = True
-            config = session.execute(query).scalar_one_or_none()
-            if config is not None:
-                enable = config.enable
-            if enable and random.random() < get_reply_rate(group_id):
+            if random.random() < get_reply_rate(group_id):
                 return True
     return False
-
-
-def set_auto_reply_enable(group_id: str, enabled: bool):
-    """
-    设置群自动回复开关
-
-    :param group_id: 群号
-    :param enabled: 是否开启
-    """
-    query = select(data.ReplyConfig).where(data.ReplyConfig.group_id.is_(group_id))
-    with database.get_session().begin() as session:
-        if session.execute(query).first() is None:
-            session.execute(
-                insert(data.ReplyConfig).values(
-                    group_id=group_id, enable=enabled, rate=AUTO_REPLY_RATE
-                )
-            )
-        else:
-            session.execute(
-                update(data.ReplyConfig)
-                .where(data.ReplyConfig.group_id.is_(group_id))
-                .values(enable=enabled)
-            )
-        session.commit()
