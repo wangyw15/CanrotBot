76d7386e2396f4a984d182dd810e0d3cd591bb6f
wangyw15 <wangyanwei15@outlook.com>
2024-08-15 23:30:26 +0800

    更改 llm 默认最长消息长度

--- plugins/llm/config.py
+++ plugins/llm/config.py
@@ -8,7 +8,7 @@ class LLMConfig(BaseModel):
     command_start: set[str] = Field(default={"/"})
     enabled: bool = Field(default=False, alias="canrot_llm_enabled")
     tempurature: float = Field(default=0.3, alias="canrot_llm_tempurature")
-    max_length: int = Field(default=2**13, alias="canrot_llm_max_length")
+    max_length: int = Field(default=2**12, alias="canrot_llm_max_length")
     backend: Literal["ollama", "openai"] = Field(
         default="ollama", alias="canrot_llm_backend"
     )
