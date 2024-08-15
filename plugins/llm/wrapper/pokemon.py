8b29637e3a9af638bb2c5c22aaa15bef7ce1f726
wangyw15 <wangyanwei15@outlook.com>
2024-08-15 19:05:08 +0800

    PokeTool 移动到独立的文件中

--- plugins/llm/wrapper/pokemon.py
+++ plugins/llm/wrapper/pokemon.py
@@ -5,19 +5,19 @@ from libraries.llm.tool import BaseTool
 from libraries.mediawiki import MediaWikiClient
 
 
-class MoegirlTool(BaseTool):
-    __description__ = "从萌娘百科搜索词条并获取最相关词条对应的页面内容，在萌娘百科上可以查到所有ACG相关的内容"
+class PokeTool(BaseTool):
+    __description__ = "从神奇宝贝百科搜索词条并获取最相关词条对应的页面内容，在神奇宝贝百科上可以查到所有神奇宝贝（宝可梦）相关的内容"
     __command__ = False
 
     def __init__(self):
         super().__init__()
-        self.client = MediaWikiClient("https://moegirl.uk/api.php")
+        self.client = MediaWikiClient("https://wiki.52poke.com/api.php")
 
     async def __call__(
         self,
         action: Annotated[
             str,
-            "search为在萌娘百科上进行搜索并获取最相关的一个页面标题；get为根据标题获取页面文本。search的结果不能直接作为回答，需要再通过get获取页面内容，根据页面内容进行回答",
+            "search为在神奇宝贝百科上进行搜索并获取最相关的一个页面标题；get为根据标题获取页面文本。search的结果不能直接作为回答，需要获取页面内容再进行回答",
         ],
         keyword: Annotated[str, "进行搜索的关键词或页面标题"],
     ) -> str:
