6784d6fa5df704d7fd023fd4f32a3b566f51db7d
wangyw15 <wangyanwei15@outlook.com>
2024-11-03 16:27:46 +0800

    修复path测试

--- tests/essentials/libraries/test_path.py
+++ tests/essentials/libraries/test_path.py
@@ -4,18 +4,18 @@ from pathlib import Path
 def test_get_asset_path():
     from canrotbot.essentials.libraries import path
 
-    assert path.get_asset_path() == Path(__file__).parent.parent.parent.parent / "assets"
-    assert path.get_asset_path("test") == Path(__file__).parent.parent.parent.parent / "assets" / "test"
+    assert path.get_asset_path().resolve() == (Path(__file__).parent.parent.parent.parent / "assets").resolve()
+    assert path.get_asset_path("test").resolve() == (Path(__file__).parent.parent.parent.parent / "assets" / "test").resolve()
 
 
 def test_get_cache_path():
     from canrotbot.essentials.libraries import path
 
-    assert path.get_cache_path() == Path(path.config.user_data_path) / "cache"
-    assert path.get_cache_path("test") == Path(path.config.user_data_path) / "cache" / "test"
+    assert path.get_cache_path().resolve() == (Path(path.config.user_data_path) / "cache").resolve()
+    assert path.get_cache_path("test").resolve() == (Path(path.config.user_data_path) / "cache" / "test").resolve()
 
 
 def test_get_data_path():
     from canrotbot.essentials.libraries import path
 
-    assert path.get_data_path("test") == Path(path.config.user_data_path) / "data" / "test"
+    assert path.get_data_path("test").resolve() == (Path(path.config.user_data_path) / "data" / "test").resolve()
