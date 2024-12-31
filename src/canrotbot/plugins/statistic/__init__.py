8bbd6f3bcfab46fd15cc502e82a58e4f3abe4fbb
wangyw15 <wangyanwei15@outlook.com>
2024-12-31 21:51:16 +0800

    statistic修复报错问题

--- src/canrotbot/plugins/statistic/__init__.py
+++ src/canrotbot/plugins/statistic/__init__.py
@@ -1,7 +1,9 @@
-from nonebot.adapters import Event
+from nonebot.adapters import Bot, Event
 from nonebot.matcher import Matcher
 from nonebot.message import event_preprocessor, run_postprocessor
-from nonebot_plugin_alconna import MsgTarget
+from nonebot_plugin_alconna import (
+    UniMessage,
+)
 from sqlalchemy import insert
 
 from canrotbot.essentials.libraries import database
@@ -10,32 +12,44 @@ from .data import MessageHistory, PluginHistory
 
 
 @event_preprocessor
-async def _(target: MsgTarget, event: Event):
-    with database.get_session().begin() as session:
-        session.execute(
-            insert(MessageHistory).values(
-                content=event.get_plaintext(),
-                private_chat=target.private,
-                channel_chat=target.channel,
-                self_id=target.self_id,
-                parent_id=target.parent_id,
-                platform_id=target.id,
+async def _(bot: Bot, event: Event):
+    try:
+        target = UniMessage.get_target(event, bot)
+        with database.get_session().begin() as session:
+            session.execute(
+                insert(MessageHistory).values(
+                    content=event.get_plaintext(),
+                    private_chat=target.private,
+                    channel_chat=target.channel,
+                    self_id=target.self_id,
+                    parent_id=target.parent_id,
+                    platform_id=target.id,
+                )
             )
-        )
+    except NotImplementedError:
+        pass
+    except Exception as e:
+        raise e
 
 
 @run_postprocessor
-async def _(target: MsgTarget, event: Event, matcher: Matcher):
-    with database.get_session().begin() as session:
-        session.execute(
-            insert(PluginHistory).values(
-                plugin_name=matcher.plugin_name,
-                module_name=matcher.module_name,
-                command=event.get_plaintext(),
-                private_chat=target.private,
-                channel_chat=target.channel,
-                self_id=target.self_id,
-                parent_id=target.parent_id,
-                platform_id=target.id,
+async def _(bot: Bot, event: Event, matcher: Matcher):
+    try:
+        target = UniMessage.get_target(event, bot)
+        with database.get_session().begin() as session:
+            session.execute(
+                insert(PluginHistory).values(
+                    plugin_name=matcher.plugin_name,
+                    module_name=matcher.module_name,
+                    command=event.get_plaintext(),
+                    private_chat=target.private,
+                    channel_chat=target.channel,
+                    self_id=target.self_id,
+                    parent_id=target.parent_id,
+                    platform_id=target.id,
+                )
             )
-        )
+    except NotImplementedError:
+        pass
+    except Exception as e:
+        raise e
