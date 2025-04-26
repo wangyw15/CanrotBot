3e76d6e9ab8e23d423af6e1f16634b89719b8d39
wangyw15 <wangyanwei15@outlook.com>
2025-04-26 17:43:39 +0800

    修复ptc小游戏双属性模式无效属性攻击的问题

--- src/canrotbot/plugins/pokemon/__init__.py
+++ src/canrotbot/plugins/pokemon/__init__.py
@@ -70,44 +70,71 @@ async def ptc_start(
     state[ROUND] = 0
     state[TYPES] = type_challenge.generate_pokemon(ptc_mode.result)
     await ptc_matcher.send(
-        "宝可梦属性黑箱挑战，开始！\n" +
-        f"当前是【{'单一' if len(state[TYPES]) == 1 else '双'}】属性模式\n" +
-        "输入【a属性】或【技能名称】进行攻击\n" +
-        "输入【属性】进行猜测\n" +
-        "输入【放弃】或【逃跑】放弃游戏"
+        "宝可梦属性黑箱挑战，开始！\n"
+        + f"当前是【{'单一' if len(state[TYPES]) == 1 else '双'}】属性模式\n"
+        + "输入【a属性】或【技能名称】进行攻击\n"
+        + "输入【属性】进行猜测\n"
+        + "输入【放弃】或【逃跑】放弃游戏"
     )
+    from nonebot import logger
+
+    logger.error(state[TYPES])
 
 
 @ptc_matcher.got("action_msg")
 async def ptc_action(state: T_State, action_msg: Message = Arg()):
     if state[ROUND] == MAX_ROUNDS:
-        await ptc_matcher.finish(f"回合数耗尽了……宝可梦黑箱逃跑了！\n正确答案是【{'+'.join(state[TYPES])}】")
+        await ptc_matcher.finish(
+            f"回合数耗尽了……宝可梦黑箱逃跑了！\n正确答案是【{'+'.join(state[TYPES])}】"
+        )
 
     action_str = action_msg.extract_plain_text()
 
     # 检查是否逃跑
     if action_str in GIVE_UP:
-        await ptc_matcher.finish(f"玩家从宝可梦属性黑箱挑战中逃跑了！\n正确答案是【{'+'.join(state[TYPES])}】")
+        await ptc_matcher.finish(
+            f"玩家从宝可梦属性黑箱挑战中逃跑了！\n正确答案是【{'+'.join(state[TYPES])}】"
+        )
 
     # 检查并设置动作和动作内容
     action = ""
     action_targets: list[str] = action_str.split()
+    type_count = len(state[TYPES])
+    target_count = len(action_targets)
     if action_str.startswith("a"):
         action = ATTACK
         action_targets = action_str[1:].split()
-        for target in action_targets:
-            if not type_challenge.check_type(target):
+        if target_count != 1:
+            await ptc_matcher.reject_arg("action_msg", "请输入一个攻击技能或属性")
+        if not type_challenge.check_type(action_targets[0]):
+            if not type_challenge.check_move(action_targets[0]):
                 await ptc_matcher.reject_arg(
-                    "action_msg", f"无效的攻击属性：{action_targets}"
+                    "action_msg", f"无效的攻击技能或属性：{action_targets[0]}"
                 )
-    elif type_challenge.check_move(action_str):
-        action = ATTACK
-    elif type_challenge.check_type(action_str):
-        action = GUESS
     else:
-        await ptc_matcher.reject_arg(
-            "action_msg", f"无效的攻击技能或属性：{action_str}"
-        )
+        if target_count == 1:
+            if type_challenge.check_move(action_targets[0]):
+                action = ATTACK
+            elif type_challenge.check_type(action_targets[0]):
+                action = GUESS if type_count == 1 else ATTACK
+            else:
+                await ptc_matcher.reject_arg(
+                    "action_msg", f"无效的攻击技能或属性：{action_str}"
+                )
+        else:
+            # 多个target只能为猜测属性
+            action = GUESS
+
+            # 单一属性模式只能猜测一个属性
+            if type_count == 1:
+                await ptc_matcher.reject_arg(
+                    "action_msg", "单一属性模式下只能猜测一个属性"
+                )
+
+            # 检查属性是否有效
+            for i in action_targets:
+                if not type_challenge.check_type(i):
+                    await ptc_matcher.reject_arg("action_msg", f"无效的属性猜测：{i}")
 
     # 更新回合数
     state[ROUND] += 1
@@ -123,16 +150,18 @@ async def ptc_action(state: T_State, action_msg: Message = Arg()):
         prompt = type_challenge.get_effectiveness_prompt(multiplier)
         await ptc_matcher.reject_arg(
             "action_msg",
-            f"【{attack_type}】属性攻击！\n" +
-            f"{prompt}\n" +
-            f"剩余回合数：{MAX_ROUNDS - state[ROUND]}/{MAX_ROUNDS}",
+            f"【{attack_type}】属性攻击！\n"
+            + f"{prompt}\n"
+            + f"剩余回合数：{MAX_ROUNDS - state[ROUND]}/{MAX_ROUNDS}",
         )
     elif action == GUESS:
         if sorted(action_targets) == sorted(state[TYPES]):
-            await ptc_matcher.finish(f"恭喜你，正确答案是【{'+'.join(state[TYPES])}】！")
+            await ptc_matcher.finish(
+                f"恭喜你，正确答案是【{'+'.join(state[TYPES])}】！"
+            )
         else:
             await ptc_matcher.reject_arg(
                 "action_msg",
-                "很遗憾，回答错误，再多尝试几次吧。\n" +
-                f"剩余回合数：{MAX_ROUNDS - state[ROUND]}/{MAX_ROUNDS}",
+                "很遗憾，回答错误，再多尝试几次吧。\n"
+                + f"剩余回合数：{MAX_ROUNDS - state[ROUND]}/{MAX_ROUNDS}",
             )
