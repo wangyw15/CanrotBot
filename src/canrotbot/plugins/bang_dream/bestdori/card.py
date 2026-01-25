e9d0768a04a61626dbd6b9c93c8ddafd1706b502
wangyw15 <wangyanwei15@outlook.com>
2026-01-25 13:09:08 +0800

    LLM tool增加bang_dream卡片信息

--- src/canrotbot/plugins/bang_dream/bestdori/card.py
+++ src/canrotbot/plugins/bang_dream/bestdori/card.py
@@ -1,21 +1,26 @@
+from canrotbot.llm.tools import register_tool
 from . import util
 
 
+@register_tool("bang_dream_get_cards_list")
 async def get_card_list() -> dict[str]:
     """
-    获取卡片列表
+    获取Bang Dream游戏中的卡片列表
 
-    :return: 卡片列表
+    Returns:
+        带有简略信息的卡片列表
     """
     return await util.bestdori_api("cards/all.5.json")
 
 
 async def get_card_info(card_id: str) -> dict[str]:
     """
-    获取卡片信息
+    获取Bang Dream游戏中指定卡片的详细信息
 
-    :param card_id: 卡片ID
+    Args:
+        card_id: 卡片ID
 
-    :return: 卡片信息
+    Returns:
+        卡片信息
     """
     return await util.bestdori_api(f"cards/{card_id}.json")
